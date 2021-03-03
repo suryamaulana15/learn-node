2d9e687b1af9bc800822740485729d0775ed6317
suryamaulana1980 <suryamaulana1980@gmail.com>
2021-03-03 22:43:09 +0700

    add html to response

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>Learn react</title></head>');
+  res.write('<body>Create Html</body>');
+  res.write('</html>');
+  res.end();
 });
 
 server.listen(3000);
