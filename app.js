2ce00d053f62e596fdf2ba22daa79de00d7e73fc
suryamaulana1980 <suryamaulana1980@gmail.com>
2021-03-03 23:02:42 +0700

    routing request

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
+  // console.log(req.url, req.method, req.headers);
+  const url = req.url;
+  if(url === '/'){
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>');
+    res.write('<body><form action="/message" method="post"> <input type="text" name="message"/><button type="submit">submit</button> </form></body>');
+    res.write('</html>');
+    return res.end();
+  }
+
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>Learn react</title></head>');
