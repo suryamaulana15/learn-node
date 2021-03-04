21c653d3484b4e0dda0e895a83fbd9f5877bc170
suryamaulana1980 <suryamaulana1980@gmail.com>
2021-03-04 19:11:43 +0700

    add message file after post

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   // console.log(req.url, req.method, req.headers);
   const url = req.url;
+  const method = req.method;
+
   if(url === '/'){
     res.write('<html>');
     res.write('<head><title>Enter Message</title></head>');
     res.write('<body><form action="/message" method="post"> <input type="text" name="message"/><button type="submit">submit</button> </form></body>');
     res.write('</html>');
+    return res.end();
+  }
+  if(url === '/message' && method === 'POST'){
+    fs.writeFileSync('message.txt', 'blablabla');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
     return res.end();
   }
 
